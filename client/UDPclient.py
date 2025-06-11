f23dd7c08820021657b8d877ddd878a6504cb8c5
Q-yuannn <3355242951@qq.com>
2025-06-12 01:06:25 +0800

    Copy codes to the client file in the new folder

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -1,3 +1,12 @@
 import socket
-
+import sys
 def main():
+    if len(sys.argv) != 4:
+       print("The command you input is invalid!")
+       sys.exit(1)
+    # extract the arguments    
+    hostname = sys.argv[0]
+    portname = sys.argv[1]
+    filename = sys.argv[2]
+    # try to open files
+    
