af179615c502d80b8f6b85d2c00198046e854a46
Q-yuannn <3355242951@qq.com>
2025-06-13 01:24:42 +0800

    Server handle the request from client.
    If request is close, send close message. If request is GET, send data message to client.

--- server/UDPserver.py
+++ server/UDPserver.py
@@ -3,6 +3,7 @@ import socket
 import threading
 import os
 import random
+import base64
 
 def handle_client_request(filename, client_address, server_socket, hostname):
     try:
@@ -18,10 +19,44 @@ def handle_client_request(filename, client_address, server_socket, hostname):
         data_port = random.randint(50000,51000)
         ok_msg = f"OK {filename} SIZE {filesize} PORT {data_port}"
         
+        # create new socket and bind it to the new port
+        data_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        data_socket.bind((hostname, data_port))
         
         # send OK message
         server_socket.sendto(ok_msg.encode(), client_address)
-
+        
+        # Open the requested file
+        with open(filename, "rb") as f:
+            while True:
+                # receive the requested data
+                request_data, address = data_socket.recvfrom(2048)
+                request = request_data.decode().strip()
+                request_parts = request.split()
+                # invalid
+                if request_data[1] != filename:
+                    continue
+                # if request is close
+                if request_parts[2] == "CLOSE":
+                    close_msg = f"FILE {filename} CLOSE_OK"
+                    # use new socket to send close message to client
+                    data_socket.sendto(close_msg.encode(), address)
+                    break
+                # if request is to get data in specfied file
+                if request_parts[2] == "GET":
+                    # get the start point and end point
+                    start = int(request_parts[4])
+                    end = int(request_parts[6])
+                    length = end - start + 1
+                    f.seek(start)
+                    block = f.read(length)
+                    # encode data 
+                    encoded = base64.b64encode(block).decode()
+                    # send the response message to client
+                    response = f"FILE {filename} OK START {start} END {end} DATA {encoded}"
+                    data_socket.sendto(response.encode(), address)
+        data_socket.close()
+        print(f"handle client requests finished")
 
     except Exception as e:
         print(f"Error: {e}")
@@ -48,5 +83,8 @@ def main():
               threading.Thread(target=handle_client_request, args=(file_name, client_address, server_socket)).start()
            else:
               print(f"Invalid request from {client_address}")
+
+if __name__ == "__main__":
+    main()
        
 
