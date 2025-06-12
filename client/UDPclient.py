36757c2814dd50bd1b81ab9843610fed03e9bdef
Q-yuannn <3355242951@qq.com>
2025-06-12 19:55:53 +0800

    Create The sendAndReceive Function

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -1,9 +1,31 @@
 import socket
 import sys
+import time
 
 def sendAndReceive(packet, socket, server_address):
-    timeout = 20
-    
+    timeout = 100
+    try_time = 0
+    maxtry_time = 30
+    while try_time < maxtry_time:
+        try:
+            socket.settimeout(timeout)
+            # send message
+            socket.sendto(packet, server_address)
+            # receieve response
+            response, server_address = socket.recvfrom(1024)
+            print(f"receive{server_address} ")
+            return response.decode('utf-8')
+        
+        # if timeout
+        except socket.timeout:
+            try_time += 1
+            print(f"waiting exceed time ({timeout}s), last try times: {timeout - try_time}")
+            if try_time < timeout:
+                time.sleep(0.5)
+                
+        except Exception as e:
+            print(e)
+            break
 
 
 def main():
