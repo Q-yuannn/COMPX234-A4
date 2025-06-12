0b7ac17b4323c3f4a80d94299546154fd35f1797
Q-yuannn <3355242951@qq.com>
2025-06-12 22:31:03 +0800

    create datagram socket to be the welcome socket and bind it to the server port from the command line

--- server/UDPserver.py
+++ server/UDPserver.py
@@ -1,7 +1,18 @@
 import sys
+import socket
+import threading
+
+
+
+
 def main():
      if len(sys.argv) != 2:
         print("Please input valid command")
         return
      # if the command is valid
      port = int(sys.argv[1])
+     # UDP socket
+     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     hostname = "WangQiyuan"
+     # bind host and port
+     server_socket.bind((hostname, port))
